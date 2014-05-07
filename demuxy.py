17c0712c79ec3442904fae8c36e83cfa95e8139c
oaxiom <37527172+oaxiom@users.noreply.github.com>
2014-05-07 12:37:00 +0800

    First working version

--- demuxy.py
+++ demuxy.py
@@ -7,19 +7,52 @@ Demuxy
 
 
 from operator import itemgetter
-import sys, os, argparse, logging, random, time, subprocess, copy, progress, numpy, itertools
+import sys, os, argparse, logging, random, time, subprocess, copy, numpy, itertools
 
+# demuxy modules
 import config
+import lib
+import progress
 
 parser = argparse.ArgumentParser(description="""demuxy is a tool to demultiplex Version: %s""" % config.version)
 parser.add_argument("-1", "--p1", help="paired-end 1 fastq file (Required)", required=True)
 parser.add_argument("-2", "--p2", help="paired-end 2 fastq file (Required)", required=True)
 parser.add_argument("-s", "--sheet", help="(Required)", required=True)
-parser.add_argument("-m", "--mismatch", help="Maximum number of mismatches in indeces (Required)", required=True)
-parser.add_argument("-o", "--output", default="unaligned_.tsv", help="output filename, reads will be split into _R1 and _R2 files (default: unaligned_.tsv)")
+parser.add_argument("-m", "--mismatch", help="Maximum number of mismatches in indeces (Required)", required=True, type=int)
+parser.add_argument("-z", "--opt", help="Profile the code for optimisation")
 
 def do(null=None):  
-    pass
+    sample_sheet = lib.load_sheet(args.sheet)
+    table = lib.populate_table(sample_sheet, args.mismatch) # 5 is the point for the first mismatch
+    
+    # open the output files:
+    files = {k: open("%s.fq" % k, "w") for k in sample_sheet.keys()}
+    counts = {i: 0 for i in sample_sheet.keys()}
+    
+    n = 0
+    m = 0
+
+    for fq1, fq2 in lib.fastqPE(args.p1, args.p2):
+        n += 1
+        index = fq1["name"].split(":")[-1]
+        # match the index against the table
+        for k in table:
+            if index in table[k]:
+                counts[k] += 1
+                files[k].write("%s\n%s\n%s\n%s\n" % (fq1["name"], fq1["seq"], fq1["strand"], fq1["qual"]))
+                files[k].write("%s\n%s\n%s\n%s\n" % (fq2["name"], fq2["seq"], fq2["strand"], fq2["qual"]))
+                break
+                
+        n += 1
+        if n > 1e6:
+            m += 1
+            n = 0
+            config.log.info("Did %sM tags" % m)
+    
+    for k in counts:
+        config.log.info("Found %s tags for sample %s" % (counts[k], k))
+    
+    return
 
 if __name__ == "__main__":    
     s = time.time() # so tmp files all get the same time
--- lib.py
+++ lib.py
@@ -4,14 +4,16 @@ Helper functions
 
 """
 
+import config
+
 def load_sheet(filename):
     """
     **Purpuse**
         Load the Illumina-style SampleSheet (Same format as for bcl2fastq)
         
         # expected format (csv):
-        FCID	Lane	SampleID	SampleRef	Index	Description	Control	Recipe	Operator	SampleProject
-        000000000-A8WTD	1	A1	hg19	TAAGGCGA-TAGATCGC	NA	N	NA	NA	cordblood_run1
+        FCID    Lane    SampleID    SampleRef   Index   Description Control Recipe  Operator    SampleProject
+        000000000-A8WTD 1   A1  hg19    TAAGGCGA-TAGATCGC   NA  N   NA  NA  cordblood_run1
     
         I only want two columns, SampleID and Index
     
@@ -25,20 +27,22 @@ def load_sheet(filename):
             sampleID = ll[2]
             index = ll[4].replace("-", "").upper()
             res[sampleID] = index
+    config.log.info("Loaded sample sheet '%s'" % filename)
     return(res)
 
-def generate_mismatches(seq, num_mismatch):
+def generate_mismatches(seq, num_mismatch, cdepth=1):
     """
     **Arguments**
         a sequence
     """
-    assert "N" not in seq, "Cannot have 'N' base pairs in the starting index" 
     res = []
     
     for idx, bp in enumerate(seq):
-        l = seq[0:idx]
-        r = seq[idx+1:]
-        print idx, seq, l + "N" + r
+        newseq = seq[0:idx] + "N" + seq[idx+1:]
+        
+        res.append(newseq)
+        if cdepth < num_mismatch:
+            res = res + generate_mismatches(newseq, num_mismatch, cdepth+1)
     return(res)
         
 def populate_table(sample_sheet, num_mismatch):
@@ -46,44 +50,109 @@ def populate_table(sample_sheet, num_mismatch):
     **Purpose**
         populate the dictionary table for sampleID and indices for a set of mismatches
     
+        The resulting table will be checked for collisions between the mismatches and some advice
+        will be given on the maximum possible num_mismatches for these indeces.
+    
     **Arguments**
         sample_sheet
             the output from load_sheet, i.e a dict of {"sampleID": "index" ... }
             
         num_mismatches 
-        
+            The number of mismatches to consider
+            
     """
     res = {}
     
     for k in sample_sheet:
         res[k] = []
         res[k].append(sample_sheet[k])
-        res[k] + generate_mismatches(sample_sheet[k], num_mismatch)
+        res[k] = res[k] + list(set(generate_mismatches(sample_sheet[k], num_mismatch)))
     
-    sanity_check_table(res)
+    res = cull_collisions(res)
+    # convert the table to sets for faster membership testing
+    for k in res:
+        res[k] = frozenset(res[k])
+    
     return(res)
     
-def sanity_check_table(pop_table):
+def cull_collisions(pop_table):
     """
     **Purpose**
-        check the population table for collisions.
+        cull colliding index matches
         
     **Arguments**
         pop_table
             the output from populate_table()
     """
-    pass
-
-def fast_matcher(seq1, seq2):
-    """
-    Some restrictions
+    # First do a quick check to see if there are any colliding members:
+    super_table = sum(pop_table.values(), [])
+    pre_set_len = len(super_table)
+    post_set_len = len(set(super_table))
     
-    1. Must be the same length.
-    2. Does not check revcomp
+    if pre_set_len == post_set_len: # no collisions, bug out
+        config.log.info("There are no collision at this mismatch level")
+        config.log.info("Populated table with '%s' combinations" % pre_set_len)
+        return(pop_table)    
+    
+    culled = 0
+    scheduled_for_deletion = []
+    # we must test each k for collsions
+    for k1 in pop_table:
+        for k2 in pop_table:
+            if k1 != k2:
+                # check for collisions between these two pairs:
+                seqs1 = set(pop_table[k1])
+                seqs2 = set(pop_table[k2])
+                union = seqs1 & seqs2
+                if union:
+                    for s in union:
+                        culled += 1
+                        # Don't delete here, as it may bias later checks for the presence of this seq
+                        scheduled_for_deletion.append(s)
+    # Now do the actual deletion:
+    for k in pop_table:
+        for s in scheduled_for_deletion:
+            if s in pop_table[k]:
+                idx = pop_table[k].index(s)
+                del pop_table[k][idx]
+
+    config.log.info("Culled %s mismatches" % (culled*2,)) # because it is culled from both lists
+    config.log.info("Populated table with '%s' combinations" % (pre_set_len-culled*2,))
+    return(pop_table)
+
+def fastqPE(filename1, filename2):
     """
-    pass
+    generator object to parse fastQ PE files
+    
+    @HWI-M00955:51:000000000-A8WTD:1:1101:13770:1659 1:N:0:NNTNNNAGNNCNCTAT
+    NGGTAAATGCGGGAGCTCCGCGCGCANNTGCGGCNNNGCATTGCCCATAATNNNNNNNCTACCGACGCTGACTNNNNNCTGTCTCTTATACACATNNNNGAGCCCACGNNNNCNNNCTAGNNNNNNNNNNNNNNNTTCTGCTTGTAAACA
+    +
+    #,,5,</<-<+++5+568A+6+5+++##5+5++5###+5+55-55A-A--5#######55+5<)+4)43++14#####*1*1*2011*0*1*1*1####***111(/'####/###-(((###############/-(/((./(((((((
+    
+    """
+    oh1 = open(filename1, "rU")
+    oh2 = open(filename2, "rU")
+
+    name1 = "dummy"
+    while name1 != "":
+        name1 = oh1.readline().strip()
+        seq1 = oh1.readline().strip()
+        strand1 = oh1.readline().strip()
+        qual1 = oh1.readline().strip()
+        
+        name2 = oh1.readline().strip()
+        seq2 = oh1.readline().strip()
+        strand2 = oh1.readline().strip()
+        qual2 = oh1.readline().strip()
+        
+        res = ({"name": name1, "strand": strand1, "seq": seq1, "qual": qual1},
+            {"name": name2, "strand": strand2, "seq": seq2, "qual": qual2})
+        yield res   
+    return
 
 if __name__ == "__main__":
     sample_sheet = load_sheet("SampleSheet_run1.csv")
-    print populate_table(sample_sheet, 2)
-        
+    populate_table(sample_sheet, 1) # 5 is the point for the first mismatch
+
+    for i, o in fastqPE("lane1_Undetermined_L001_R1_001.fastq", "lane1_Undetermined_L001_R2_001.fastq"):
+        pass
